89f9eddd794216846c76d7f9e5e8714e5854838d
Admin <Admin@10.100.230.176>
2023-05-17 18:17:33 +0200

    el ejercicio 4 me confunde

--- TEMA10_FICHEROS/src/ejercicios/Ejercicio04.java
+++ TEMA10_FICHEROS/src/ejercicios/Ejercicio04.java
@@ -1,23 +1,48 @@
 package ejercicios;
 
 import java.io.*;
+import java.util.Scanner;
 
 public class Ejercicio04 {
 
 	public static void main(String[] args) {
 		String cadena = "";
 		
+		BufferedWriter bw = null;
+		BufferedReader br = null;
+		
 		try {
-			BufferedWriter bw = new BufferedWriter(new FileWriter("escribir-ej4"));
-			BufferedReader br = new BufferedReader(new FileReader("escribir-ej4"));
+			bw = new BufferedWriter(new FileWriter("escribir-ej4"));
+			br = new BufferedReader(new FileReader("escribir-ej4"));
+			Scanner sc = new Scanner(System.in);
 			
+			System.out.println("Introduzca unas líneas de texto: ");
+			cadena = sc.nextLine();
 			
+			bw.write(cadena);
 			
 			while(!cadena.equalsIgnoreCase("fin")) {
+				cadena = br.readLine();
+				
+				bw.newLine();
+				
+				cadena = sc.nextLine();
+				
 				bw.write(cadena);
 			}
+			br.close();
+			sc.close();
 		} catch (IOException e) {
-			System.err.println("Fichero no encontrado");
+			System.err.println("Error");
+			System.out.println(e.getMessage());
+		} finally {
+			try {
+				bw.flush();
+				bw.close();
+			} catch (IOException e) {
+				System.err.println("Error");
+				System.out.println(e.getMessage());
+			}
 		}
 	}
 
