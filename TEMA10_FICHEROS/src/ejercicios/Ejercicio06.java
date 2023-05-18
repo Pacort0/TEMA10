30d88ac2578a4fa21de8c569c34ac5a94bc825eb
Admin <Admin@10.100.224.94>
2023-05-18 19:13:29 +0200

    he conseguido hacer el 7 estoy orgulloso

--- TEMA10_FICHEROS/src/ejercicios/Ejercicio06.java
+++ TEMA10_FICHEROS/src/ejercicios/Ejercicio06.java
@@ -7,28 +7,35 @@ public class Ejercicio06 {
 
 	public static void main(String[] args) {
 		TreeSet<Integer> listaNums = new TreeSet<>();
-		BufferedWriter bw;
-		
+		BufferedWriter bw = null;
+
 		try {
 			Scanner sc = new Scanner(new FileReader("numsNoOrden"));
-			
-			while(sc.hasNextInt()) {
+
+			while (sc.hasNextInt()) {
 				listaNums.add(sc.nextInt());
 			}
-			
+
 			bw = new BufferedWriter(new FileWriter("numsEnOrden"));
-			
-			for(Integer ts : listaNums) {
+
+			for (Integer ts : listaNums) {
 				bw.write(Integer.toString(ts));
 				bw.write(" ");
 			}
-			
+
 			sc.close();
-			bw.close();
 		} catch (FileNotFoundException e) {
 			System.err.println("El fichero indicado no existe");
 		} catch (IOException e) {
 			System.err.println("Error de entrada/salida");
+		} finally {
+			try {
+				bw.flush();
+				bw.close();
+				
+			} catch (IOException e) {
+				System.err.println("Error de entrada/salida");
+			}
 		}
 	}
 
--- TEMA10_FICHEROS/src/ejercicios/Ejercicio07.java
+++ TEMA10_FICHEROS/src/ejercicios/Ejercicio07.java
@@ -12,43 +12,79 @@ public class Ejercicio07 {
 		int opcion = 0;
 		String nombre;
 		long tlfn;
+		BufferedWriter bw;
+		String cadena;
+		String[] separado = new String[0];
 		
 		try {
-			BufferedWriter bw = new BufferedWriter(new FileWriter("agenda.txt"));
+			BufferedReader br = new BufferedReader(new FileReader("agenda.txt"));
+			
+			cadena = br.readLine();
+			
+			while(cadena != null) {
+				
+				separado = cadena.split(" --> ");
+				
+				agenda.put(separado[0], Long.parseLong(separado[1]));
+				
+				cadena = br.readLine();
+			}
+			
+			br.close();
+			
+		} catch (FileNotFoundException e1) {
+			System.err.println("Fichero no encontrado");
 		} catch (IOException e) {
 			System.err.println("Error de entrada/salida");
-		}	
+		}
+		
 		
 		do {
 			menu();
 			opcion = SC.nextInt();
 			SC.nextLine();
-			
+
 			switch (opcion) {
 			case 1:
 				System.out.println("Introduzca el nombre del nuevo contacto: ");
 				nombre = SC.nextLine();
 				System.out.println("Introduzca el número de teléfono del nuevo contacto: ");
 				tlfn = SC.nextLong();
-				
+
 				if (!agendaCompleta() && !agenda.containsKey(nombre)) {
 					agenda.put(nombre, tlfn);
-					
+
 				} else {
 					System.err.println("La agenda está completa");
 				}
 				break;
 			case 2:
 				System.out.println("Introduzca el nombre del contacto a buscar: ");
-				if(buscaContacto(SC.nextLine()) != -1) {
-					System.out.println(buscaContacto(SC.nextLine()));
+				nombre = SC.nextLine();
+				
+				if(buscaContacto(nombre)) {
+					System.out.println("Teléfono: " + agenda.get(nombre));
 				} else {
 					System.err.println("El contacto no existe");
 				}
 				break;
 			case 3:
-				System.out.println(agenda);
+				if (agenda.size() == 0) {
+					System.err.println("La agenda está vacía");
+				} else {
+					System.out.println(agenda);
+				}
 				break;
+			case 4:
+				try {
+					bw = new BufferedWriter(new FileWriter("agenda.txt"));
+					
+					escribe(bw);
+					
+					bw.close();
+				} catch (IOException e) {
+					System.err.println("Error de entrada/salida");
+				}
 			}
 		} while (opcion != 5);
 	}
@@ -69,16 +105,28 @@ public class Ejercicio07 {
 		return completa;
 	}
 
-	public static long buscaContacto(String nombre) {
-		long num;
+	public static boolean buscaContacto(String nombre) {
+		boolean existe = false;
 
-		if (agenda.containsKey(nombre)) {
-			num = -1;
-		} else {
-			num = agenda.get(nombre);
+		if(agenda.containsKey(nombre)) {
+			existe = true;
 		}
 
-		return num;
+		return existe;
+	}
+	
+	public static void escribe(BufferedWriter bw) {
+		String cadena;
+		
+		for(String llave : agenda.keySet()) {
+			cadena = llave + " --> " + agenda.get(llave);
+			try {
+				bw.write(cadena);
+				bw.newLine();
+			} catch (IOException e) {
+				System.err.println("Error de entrada/salida");
+			}
+		}
 	}
 
 }
