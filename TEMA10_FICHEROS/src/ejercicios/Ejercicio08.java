696eaf6e484d6d043db0cd34ce257c46118f77ca
frodriguez <frodriguez@106-24.info2>
2023-05-23 12:37:24 +0200

    a<

--- TEMA10_FICHEROS/src/ejercicios/Ejercicio08.java
+++ TEMA10_FICHEROS/src/ejercicios/Ejercicio08.java
@@ -6,8 +6,6 @@ import java.util.*;
 public class Ejercicio08 {
 	
 	final static Scanner SC = new Scanner(System.in);
-	public static HashMap<Date, Integer> fechaYmax = new HashMap<>();
-	public static HashMap<Date, Integer> maxYmin = new HashMap<>();
 
 	public static void main(String[] args) {
 		
